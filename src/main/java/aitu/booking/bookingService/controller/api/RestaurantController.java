d2ea7d7064671de1e2e1baa750fdef26627d345f
Timur Umurzakov <goust327@gmail.com>
2023-04-25 09:21:50 +0600

    Changed return

--- src/main/java/aitu/booking/bookingService/controller/api/RestaurantController.java
+++ src/main/java/aitu/booking/bookingService/controller/api/RestaurantController.java
@@ -1,23 +1,24 @@
 package aitu.booking.bookingService.controller.api;
 
 import aitu.booking.bookingService.controller.BaseController;
-import aitu.booking.bookingService.dto.create.CreateBookingDTO;
 import aitu.booking.bookingService.dto.create.CreateMenuDTO;
 import aitu.booking.bookingService.dto.create.CreateRestaurantDTO;
 import aitu.booking.bookingService.dto.responses.ResponseSuccessWithData;
 import aitu.booking.bookingService.dto.restaurant.RestaurantSmallDTO;
 import aitu.booking.bookingService.exception.ApiException;
-import aitu.booking.bookingService.model.Booking;
 import aitu.booking.bookingService.model.Menu;
 import aitu.booking.bookingService.model.Restaurant;
 import aitu.booking.bookingService.service.RestaurantService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.security.core.Authentication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.management.InstanceNotFoundException;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/restaurant")
 public class RestaurantController extends BaseController {
@@ -31,22 +32,24 @@ public class RestaurantController extends BaseController {
 
 
     @GetMapping("/{id}")
-    public ResponseSuccessWithData<RestaurantSmallDTO> getRestaurant(@PathVariable Long id) {
+    public ResponseEntity<ResponseSuccessWithData<RestaurantSmallDTO>> getRestaurant(@PathVariable Long id) {
         try {
             RestaurantSmallDTO restaurant = restaurantService.getRestaurantSmallById(id);
-            return new ResponseSuccessWithData<>(restaurant);
+            return ResponseEntity.ok(new ResponseSuccessWithData<>(restaurant));
         } catch (InstanceNotFoundException ex) {
-            throw new ApiException("404", ex.getMessage());
+            log.error("Error: {}", ex.getMessage(), ex);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 
     @GetMapping("/{id}/menu")
-    public ResponseSuccessWithData<Menu> getRestaurantMenu(@PathVariable Long id) {
+    public ResponseEntity<ResponseSuccessWithData<Menu>> getRestaurantMenu(@PathVariable Long id) {
         try {
             Menu menu = restaurantService.getRestaurantMenu(id);
-            return new ResponseSuccessWithData<>(menu);
+            return ResponseEntity.ok(new ResponseSuccessWithData<>(menu));
         } catch (InstanceNotFoundException | IndexOutOfBoundsException ex) {
-            throw new ApiException("404", ex.getMessage());
+            log.error("Error: {}", ex.getMessage(), ex);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 
